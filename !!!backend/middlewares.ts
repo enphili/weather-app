a08a04424889b9b929041019ae8653e9799d7d1f
Konstantin Pokazanyev <41166090+enphili@users.noreply.github.com>
2023-03-26 16:50:59 +0500

    Delete !!!backend directory

--- !!!backend/middlewares.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from 'express'
-
-export const responseHeaders = function(req: Request, res: Response, next: NextFunction) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, Access-Control-Allow-Headers, Authorization")
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-  next()
-}
